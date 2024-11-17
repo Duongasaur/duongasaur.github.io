0704951378365cb9cd0ae9786975f6e1764dfcda
Daniel Duong <daniel.duong@derivco.com.au>
2024-11-17 23:00:47 +1000

    gh portfolio first commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dan's Portfolio</h1>
+        <p>
+          Welcome to my portfolio! I'm Dan, a software engineer.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/dan-devs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin Profile
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
